9ad75d72cbb2db6b404336f164a6caf83ef16a4f
mitachan1604 <minhtrang.bkc@gmail.com>
2024-03-07 22:58:50 +0700

    cau 3

---
+++ c3/models/UserModel.php
@@ -0,0 +1,94 @@
+<?php
+
+require_once 'BaseModel.php';
+
+class UserModel extends BaseModel {
+
+    public function findUserById($id) {
+        $sql = 'SELECT * FROM users WHERE user_id = '.$id;
+        $user = $this->select($sql);
+
+        return $user;
+    }
+
+    public function findUser($keyword) {
+        $sql = 'SELECT * FROM users WHERE user_name LIKE %'.$keyword.'%'. ' OR user_email LIKE %'.$keyword.'%';
+        $user = $this->select($sql);
+
+        return $user;
+    }
+
+    /**
+     * Authentication user
+     * @param $userName
+     * @param $password
+     * @return array
+     */
+    public function auth($userName, $password) {
+        $md5Password = md5($password);
+        $sql = 'SELECT * FROM users WHERE user_name = "' . $userName . '" AND user_pass = "'.$md5Password.'"';
+
+        $user = $this->select($sql);
+        return $user;
+    }
+
+    /**
+     * Delete user by id
+     * @param $id
+     * @return mixed
+     */
+    public function deleteUserById($id) {
+        $sql = 'DELETE FROM users WHERE user_id = '.$id;
+        return $this->delete($sql);
+
+    }
+
+    /**
+     * Update user
+     * @param $input
+     * @return mixed
+     */
+    public function updateUser($input) {
+        $sql = 'UPDATE users SET 
+                 user_name = "' . mysqli_real_escape_string(self::$_connection, $input['name']) .'", 
+                 user_pass="'. md5($input['password']) .'"
+                WHERE user_id = ' . $input['id'];
+
+        $user = $this->update($sql);
+
+        return $user;
+    }
+
+    /**
+     * Insert user
+     * @param $input
+     * @return mixed
+     */
+    public function insertUser($input) {
+        $sql = "INSERT INTO `users` (`user_name`, `user_pass`) VALUES (" .
+                "'" . $input['name'] . "', '".md5($input['password'])."')";
+
+        $user = $this->insert($sql);
+
+        return $user;
+    }
+
+    /**
+     * Search users
+     * @param array $params
+     * @return array
+     */
+    public function getUsers($params = []) {
+        //Keyword
+        if (!empty($params['keyword'])) {
+            $sql = 'SELECT * FROM users WHERE user_name LIKE "%' . $params['keyword'] .'%" AND (user_email LIKE "%@gmail.com%" or user_email LIKE "%@outlook.com%" or user_email LIKE "%@yahoo.ca%")';
+
+            //Get data
+            $users = $this->query($sql);
+        } else {
+            $sql = 'SELECT * FROM users WHERE user_email LIKE "%@gmail.com%" or user_email LIKE "%@outlook.com%" or user_email LIKE "%@yahoo.ca%"';
+            $users = $this->select($sql);
+        }
+        return $users;
+    }
+}
